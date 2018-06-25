ef327dde19bd8794f4be7e7ed14b5dc9cf115014
Shriney Malekar <shriney.malekar.gmail@gmail.com>
2018-06-26 00:48:28 +0530

    Order Component created

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,17 @@ import classes from './Burger.css';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map(igkey => {
+    let transformedIngredients = Object.keys(props.ingredients).map(igkey => {
         return [...Array(props.ingredients[igkey])].map((_,i) => {
-            console.log(_);
             return <BurgerIngredients key={igkey+i} type={igkey} />
         })
-    });
+    }).reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredients type="bread-top" />
